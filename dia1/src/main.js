691bdf0145d9c82374febb0ec2f48d0a21e60d61
Luan Bonetto <luanbonetto42@gmail.com>
2021-08-20 16:12:21 -0300

    feat: criando link com função para ocultar ou exibir o conteúdo na página

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,4 +1,36 @@
-document.querySelector('.app').innerHTML = `
-  <h1>B. Academy</h1>
-  <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
-`
+const divApp = document.querySelector('.app')
+let isVisible = true
+
+const insertContent = () => {
+  divApp.innerHTML = `
+    <h1>B. Academy</h1>
+    <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
+  `
+}
+
+const removeContent = () => {
+  while(divApp.firstChild){
+    divApp.removeChild(divApp.firstChild)
+  }
+}
+
+const setButtonVisibilityText = () => {
+  const buttonVisibility = document.querySelector('.button-visibility')
+  if(isVisible){
+    buttonVisibility.textContent = 'Clique aqui para ocultar!'
+  } else {
+    buttonVisibility.textContent = 'Clique aqui para exibir!'
+  }
+}
+
+const setVisibility = () => {
+  isVisible = !isVisible
+  isVisible ? insertContent() : removeContent()
+  setButtonVisibilityText()
+}
+
+isVisible && insertContent()
+
+divApp.insertAdjacentHTML('afterend', `
+  <a class="button-visibility" onclick={setVisibility()}>Clique aqui para ocultar!</a>
+`)
