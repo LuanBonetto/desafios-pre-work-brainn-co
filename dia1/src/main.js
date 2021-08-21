34acb1d590423d27a991a798c46c85f24d5cf629
Luan Bonetto <luanbonetto42@gmail.com>
2021-08-21 04:26:50 -0300

    removendo código desnecessário

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,26 +1,23 @@
 import './style.css'
 
-(function () {
-  const divApp = document.querySelector('[data-js="app"]')
-  let isVisible = true
+const divApp = document.querySelector('[data-js="app"]')
+let isVisible = true
 
-  divApp.innerHTML = `
-    <h1>B. Academy</h1>
-    <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
-  `
+divApp.innerHTML = `
+  <h1>B. Academy</h1>
+  <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
+`
 
-  divApp.insertAdjacentHTML('afterend', `
+divApp.insertAdjacentHTML('afterend', `
     <a href="#" class="button-visibility" data-js="link">Clique aqui para ocultar!</a>
-  `)
+`)
 
-  const link = document.querySelector('[data-js="link"]')
+const link = document.querySelector('[data-js="link"]')
 
-  link.addEventListener('click', (event) => {
-    event.preventDefault()
+link.addEventListener('click', (event) => {
+  event.preventDefault()
 
-    isVisible = !isVisible
-    link.textContent = isVisible ? 'Clique aqui para ocultar!' : 'Clique aqui para exibir!'
-    divApp.style.display = isVisible ? 'block' : 'none'
-  })
-
-}())
+  isVisible = !isVisible
+  link.textContent = isVisible ? 'Clique aqui para ocultar!' : 'Clique aqui para exibir!'
+  divApp.style.display = isVisible ? 'block' : 'none'
+})
